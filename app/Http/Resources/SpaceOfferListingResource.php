edccdd03884c90d8ba5cd6f70af39ccd83d07198
Nick Ezekiel <nickezekias@gmail.com>
2025-03-23 23:27:48 +0100

    feat: enhance SpaceOfferListingResource with additional user details and timestamps

--- app/Http/Resources/SpaceOfferListingResource.php
+++ app/Http/Resources/SpaceOfferListingResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Helpers\Initials;
 
 class SpaceOfferListingResource extends JsonResource
 {
@@ -18,12 +19,14 @@ class SpaceOfferListingResource extends JsonResource
         $owner = $this->user();
         $user = [
             'firstName' => $owner->first_name,
+            'fullName' => "$owner->first_name $owner->last_name",
             'lastName' => $owner->last_name,
             'id' => $owner->id,
-            'initials' => $owner->initials,
+            'initials' => Initials::generate("$owner->last_name $owner->first_name"),
         ];
 
         return [
+            'createdAt' => $this->created_at,
             'id' => $this->id,
             'availableSpaceDimensions' => $this->available_space_dimensions,
             'availableWeight' => $this->available_weight,
@@ -39,6 +42,8 @@ class SpaceOfferListingResource extends JsonResource
             'itemRestrictions' => $this->item_restrictions,
             'pricePerUnit' => $this->price_per_unit,
             'specialInstructions' => $this->special_instructions,
+            'status' => $this->status,
+            'updatedAt' => $this->updated_at,
             'userId' => $this->user_id,
             'user' => $user,
             'weightUnit' => $this->weightUnit,
